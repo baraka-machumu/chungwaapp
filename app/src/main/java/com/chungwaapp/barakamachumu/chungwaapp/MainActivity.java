6d5d0dd86ce4879a06bcdf60ee93a4f7d3250c84
fatuma.mkima <fettymkima@gmail.com>
2018-08-28 13:24:03 +0300

    main activity changes

--- app/src/main/java/com/chungwaapp/barakamachumu/chungwaapp/MainActivity.java
+++ app/src/main/java/com/chungwaapp/barakamachumu/chungwaapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chungwaapp.barakamachumu.chungwaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+//textView.setOnClickListener(new View.OnClickListener(){
+//        public void onClick(View v)
+//        {
+//            Intent intent = new Intent(this, RegisterActivity.class);
+//            startActivity(intent);
+//        }
+//    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,4 +53,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
